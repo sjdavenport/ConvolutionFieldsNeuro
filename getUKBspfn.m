dd6e91f74292efb7be9c08696d1f1fb5362db40f
BrainStatsSam <12SDavenport@gmail.com>
2020-09-01 11:34:03 +0100

    backup

--- getUKBspfn.m
+++ getUKBspfn.m
@@ -19,11 +19,14 @@ function spfn = getUKBspfn( RSfolder, do_gaussianize, mask )
 % EXAMPLES
 % %% Resting State Data example
 % mask = imgload('MNImask');
-% spfn = getUKBspfn( 'R2Block', 1, mask );
+% spfn = getUKBspfn( 'R2Block', 0, mask );
 % random_subset = spfn(4)
 % subset = [1,3,5]; spec_subset = spfn(subset);
 % subs = loadUKB([1,3,5], 'R2Block', 'cope', 'copemask');
-% isequal(spec_subset, subs);
+% isequal(spec_subset, subs)
+% 
+% % Load the spfn which generates subjects on their intersection masks
+% spfn = getUKBspfn( 'R2Block', 1, 'intersect' )
 %--------------------------------------------------------------------------
 % AUTHOR: Samuel Davenport
 %--------------------------------------------------------------------------
@@ -75,7 +78,7 @@ function lat_data = get_sample_fields_nifti_intersect(subset2use, do_gaussianize
     intersection_mask = prod((mask_fields.field > 0.5),4);
     
     % Obtain the lattice data (including bounding the mask
-    lat_data = loadUKB( subset2use, RSfolder, 'cope', drawset, intersection_mask, RSfolder);
+    lat_data = loadUKB( subset2use, RSfolder, 'cope', drawset, intersection_mask);
     if do_gaussianize
         lat_data = Gaussianize(lat_data);
     end
